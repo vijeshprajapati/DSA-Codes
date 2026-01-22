9e93118d5592e46a0349fed65a03339969b73ec9
Vijesh <vijeshprajapati.2002@gmail.com>
2026-01-23 00:07:03 +0530

    added spaces

--- Dynamic Programming/ClimbingStairs.cpp
+++ Dynamic Programming/ClimbingStairs.cpp
@@ -1,7 +1,9 @@
 /*
 Gfg link : https://www.geeksforgeeks.org/dsa/count-ways-reach-nth-stair/
 Problem Statement: 
-There are n stairs, and a person standing at the bottom wants to climb stairs to reach the top. The person can climb either 1 stair or 2 stairs at a time, the task is to count the number of ways that a person can reach at the top.
+There are n stairs, and a person standing at the bottom wants to climb stairs to reach the top. 
+The person can climb either 1 stair or 2 stairs at a time, the task is to count the number of 
+ways that a person can reach at the top.
 
 --------------------------------------------------------
 Only recursive approach is written here
